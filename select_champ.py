7eaf79d352532da9cb15f6ae36f54c556c698c04
Evan Loughlin <emloughl@gmail.com>
2023-01-08 20:09:45 -0700

    Add chatting and game_started metrics

--- select_champ.py
+++ select_champ.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     CLI=argparse.ArgumentParser()
     CLI.add_argument(
     "--champs", 
+    nargs="*",
     type=str,
     default=[],  
     )
@@ -27,7 +28,6 @@ if __name__ == "__main__":
     type=str,  
     default=None,
     )
-    args = CLI.parse_args()
     CLI.add_argument(
     "--fact",
     type=bool,  
@@ -40,6 +40,13 @@ if __name__ == "__main__":
         exit("You must select at least 1 champ to select.")
     if len(args.bans) == 0:
         exit("You must select at least 1 champ to ban.")
+
+    if args.insult:
+        print("Random Insult:")
+        print(generate_random_insult(args.insult))
+    if args.fact:
+        print("Random Fact:")
+        print(generate_random_fact())
 
     print("Champs (Order of Priority): %r" % args.champs)
     print("Bans (Order of Priority): %r" % args.bans)
@@ -59,16 +66,16 @@ if __name__ == "__main__":
         print("Regaining focus on League Screen...")
         # Ensure focus is regained 
         for _ in range(5):
-            x, _ = wait_until_img_appears_or_dodge_occurs(["top_bar.png"], math.inf)
+            x, _ = wait_until_img_appears_or_dodge_occurs(["top_bar.png"], 1)
             gui.click(x)
-            time.sleep(1)
+            time.sleep(0.5)
 
         print("Waiting for champ pre-select...")
         # Pre-Select your Champ
         x, dodged = wait_until_img_appears_or_dodge_occurs(["search.png"], math.inf)
         if dodged: 
             continue
-        gui.click(x, interval=rt())
+        gui.click(x, clicks=2)
         gui.write(args.champs[0], interval=random.uniform(0.1, 0.25))
         x, dodged = wait_until_img_appears_or_dodge_occurs(["champ_box.png"], 3)
         if dodged: 
@@ -90,8 +97,8 @@ if __name__ == "__main__":
             x, dodged = wait_until_img_appears_or_dodge_occurs(["search.png"], math.inf)
             if dodged: 
                 break
-            gui.click(x, interval=rt())
-            gui.press('backspace', presses=25, interval=0.07)
+            gui.click(x, clicks=2)
+            gui.press('backspace')
             gui.write(args.bans[0], interval=random.uniform(0.05, 0.1))
             x, dodged = wait_until_img_appears_or_dodge_occurs(["ban_border.png"], 2)
             if dodged: 
@@ -108,6 +115,8 @@ if __name__ == "__main__":
                 continue
             gui.click(x, interval=rt())
             print("Champ {} Banned...".format(ban))
+            break
+
         if dodged:
             continue
 
@@ -120,11 +129,11 @@ if __name__ == "__main__":
             x, dodged = wait_until_img_appears_or_dodge_occurs(["search.png"], math.inf)
             if dodged: 
                 break
-            gui.click(x, interval=rt())
-            gui.press('backspace', presses=25, interval=random.uniform(0.05,0.1))
+            gui.click(x, clicks=2)
+            gui.press('backspace')
             gui.write(champ, interval=random.uniform(0.5,0.13))
 
-            x, dodged = wait_until_img_appears_or_dodge_occurs(["champ_box.png"], 2)
+            x, dodged = wait_until_img_appears_or_dodge_occurs(["champ_box.png"], max_tries=2)
             gui.click(x, interval=rt())
             if dodged:
                 break
@@ -132,7 +141,7 @@ if __name__ == "__main__":
                 print("Could not select {}".format(champ))
                 continue
 
-            x, dodged = wait_until_img_appears_or_dodge_occurs(["lock_in.png"], 2)
+            x, dodged = wait_until_img_appears_or_dodge_occurs(["lock_in.png"], max_tries=2)
             if dodged: 
                 break
             if not x:
@@ -141,6 +150,7 @@ if __name__ == "__main__":
 
             print("Locked In!")
             gui.click(x, interval=rt())
+            break
         if dodged:
             continue
 
@@ -151,6 +161,9 @@ if __name__ == "__main__":
             gui.click(x)
             gui.write(generate_random_insult(args.insult), random.uniform(0.05, 0.15))
             gui.press("enter")
+
+        x, dodged = wait_until_img_appears_or_dodge_occurs(["top_bar.png"], math.inf)
+        gui.click(x)
 
         if args.fact:
             x, dodged = wait_until_img_appears_or_dodge_occurs(["chat.png"], math.inf)
